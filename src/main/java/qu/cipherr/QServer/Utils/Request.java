280da2a472658ef51f025a608d2083e60cee84dc
Hoomie (Quantomized) <120099354+qu-cipher@users.noreply.github.com>
2024-06-20 18:19:03 +0330

    Create Request.java

---
+++ src/main/java/qu/cipherr/QServer/Utils/Request.java
@@ -0,0 +1,21 @@
+package qu.cipherr.QServer.Extra;
+
+public class Request {
+
+    public static String getType(String requestLine) {
+        if (requestLine != null && !requestLine.isEmpty()) {
+            return requestLine.split(" ")[0];
+        }
+        return "None";
+    }
+
+    public static String getPath(String requestLine) {
+        if (requestLine != null && !requestLine.isEmpty()) {
+            String[] parts = requestLine.split(" ");
+            if (parts.length > 1) {
+                return parts[1];
+            }
+        }
+        return "/";
+    }
+}
