de2e9a7f18c63f666575e40b0e6c1dca171272e0
QuantumCipherr <120099354+qu-cipher@users.noreply.github.com>
2024-06-22 23:53:57 +0330

    add getHeaders()

--- src/main/java/qu/cipherr/QServer/Objects/Request.java
+++ src/main/java/qu/cipherr/QServer/Objects/Request.java
@@ -1,5 +1,12 @@
 package qu.cipherr.QServer.Objects;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Request {
 
     public static String getType(String requestLine) {
@@ -17,5 +24,22 @@ public class Request {
             }
         }
         return "/";
+    }
+
+    public static Map getHeaders(Socket client) throws IOException {
+        StringBuffer result = new StringBuffer();
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        Map<String, String> headers = new HashMap<>();
+        String headerLine;
+
+        while ((headerLine = in.readLine()) != null && !headerLine.isEmpty()) {
+            String[] headerParts = headerLine.split(":", 2);
+            if (headerParts.length == 2) {
+                headers.put(headerParts[0].trim(), headerParts[1].trim());
+            }
+        }
+
+        return headers;
     }
 }
